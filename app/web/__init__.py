50bee34751a2785879ef84c0a21108ed04a6fb5f
adesokandijam <adesokanmajid@gmail.com>
2021-11-22 09:59:02 +0100

    Third Commit

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dijam:dijam@postgre:5432/dijam'
 app.config['SECRET_KEY'] = 'HARD TO GUESS THE STRING'
 db =SQLAlchemy(app)
--- app/web/routes.py
+++ app/web/routes.py
@@ -1,6 +1,17 @@
-from web import app
+from flask.helpers import url_for
+from web import app, db
+from flask import render_template, redirect
+from web.forms import MyForm
+from web.models import User
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home_page():
-    return "See me see trouble"
+    form = MyForm()
+    if form.validate_on_submit():
+        u1 = User(name = form.name.data)
+        db.session.add(u1)
+        db.session.commit()
+        return redirect(url_for('home_page'))
+    users = User.query.all()
+    return render_template('base.html', form = form, users = users)
